4d76d214eb0cedf1bb06ea8f6a3916bd037b674b
Rusal Shrestha <rusalo101@gmail.com>
2021-05-18 23:04:14 -0400

    added functionality to open/close the cart modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { Fragment } from "react";
+import { Fragment , useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [ cartIsShown, setCartIsShown ] = useState(false);
+
+  const showCartHandler = (cartIsShown) => {
+    setCartIsShown(true )
+  };
+
+  const hideCartHandler = (cartIsShown) => {
+    setCartIsShown(false)
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown ? <Cart onClose={hideCartHandler} /> : null}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
